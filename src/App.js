d80242cf98d3790cf96c6cc5935fc5e623823d66
Panagiotis Anagnostaras <anagnostaras.p@gmail.com>
2024-11-23 15:41:24 +0100

    cleaning

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import Home from './pages/Home'
 import AboutMe from './pages/AboutMe';
 import Projects from './pages/Projects';
 import Pendulum from './pages/Projects/Pendulum';
-import School from './pages/School';
 import Navbar from './components/Navbar';
-import Cmake from './pages/School/Cmake';
-import Networks from './pages/School/Networks'
 
 function App() {
   return (
@@ -18,10 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="aboutme" element={<AboutMe />} />
         <Route path="projects" element={<Projects />} />
-        <Route path="School" element={<School />} />
         <Route path="projects/pendulum" element={<Pendulum />} />
-        <Route path="school/cmake" element={<Cmake />} />
-        <Route path="school/networks" element={<Networks />} />
       </Routes>
     </div>
     </>

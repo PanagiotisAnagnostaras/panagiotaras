96caae08210fc0aa9d95ca0cf8501ab0185b980e
Panagiotis Anagnostaras <anagnostaras.p@gmail.com>
2025-01-12 11:25:39 +0100

    .

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -8,35 +8,43 @@ const Timeline = () => {
         <div className="timeline-event">
           <div className="event-year">8/2023-now</div>
           <div className="event-description">
-            <h3>Robotics software engineer, Kemaro AG, Eschlikon Switzerland</h3>
+            <h1>Robotics software engineer</h1>
+            <h2>Kemaro AG</h2>
+            <h3>Eschlikon Switzerland</h3>
           </div>
         </div>
 
         <div className="timeline-event">
           <div className="event-year">11/2021-7/2022</div>
           <div className="event-description">
-            <h3>Research and Development Intern, Hitachi Energy, Zurich Switzerland</h3>
+            <h1>Research and Development Intern</h1>
+            <h2>Hitachi Energy</h2>
+            <h3>Zurich Switzerland</h3>
           </div>
         </div>
 
         <div className="timeline-event">
           <div className="event-year">9/2020-5/2023</div>
           <div className="event-description">
-            <h3>Master of Science, Robotics, Systems and Control, ETH Zurich</h3>
+            <h1>Master of Science: Robotics, Systems and Control</h1>
+            <h3>ETH Zurich</h3>
           </div>
         </div>
 
         <div className="timeline-event">
           <div className="event-year">7/2018-9/2018</div>
           <div className="event-description">
-            <h3>Industrial Mechanical Engineer Intern, Karelia Tobacco Company, Kalamata, Greece</h3>
+            <h1>Mechanical Engineer Intern</h1>
+            <h2>Karelia Tobacco Company</h2>
+            <h3>Kalamata, Greece</h3>
           </div>
         </div>
 
         <div className="timeline-event">
           <div className="event-year">9/2014-9/2019</div>
           <div className="event-description">
-            <h3>Diploma in Mechanical Engineering, AUTH Greece</h3>
+            <h1>Diploma in Mechanical Engineering</h1>
+            <h3>AUTH Greece</h3>
           </div>
         </div>
 
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,10 +1,13 @@
 import './page.css'
-import thinker from "../images/thinker.jpg"
+import salkantay from "../images/salkantay.jpg"
 import Timeline from '../components/Timeline'
 let AboutMe = () => {
     return (<div>
         <h2>About me</h2>
-        <img src={thinker} alt="Thinker" class="image-resized"></img>
+        <figure>
+            <img src={salkantay} alt="salkantay" class="image-resized" />
+            <figcaption>Humantay lake, Peru.</figcaption>
+        </figure>
         <hr />
         <a className='link-style' href="CV.pdf" target="_blank" download>Download CV</a>
         <a className='link-style' href="https://www.linkedin.com/in/panagiotis-anagnostaras-1aa7b5155/" target="_blank">Linkedin account</a>

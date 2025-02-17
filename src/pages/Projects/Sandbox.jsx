6e9cf6b1163d2bdffb5a9573c973eb696000523d
Panagiotis Anagnostaras <anagnostaras.p@gmail.com>
2025-02-17 23:31:36 +0100

    added dl repo

--- src/pages/Projects/Sandbox.jsx
+++ src/pages/Projects/Sandbox.jsx
@@ -5,8 +5,9 @@ let Sandbox = () => {
     return (
         <div>
             <h2>Sandbox</h2>
-            <a className='link-style' href="https://github.com/PanagiotisAnagnostaras/sandbox" target="_blank">Github repo</a>
-            <p>This project is about building a sandbox for trying things in Python, C++, ROS and algorithms.</p>
+            <a className='link-style' href="https://github.com/PanagiotisAnagnostaras/sandbox" target="_blank">Github repo general</a>
+            <a className='link-style' href="https://github.com/PanagiotisAnagnostaras/deeplearningPlayground/tree/main" target="_blank">Github repo deeplearning</a>
+            <p>This project is about building sandboxs for trying things in Python, C++, ROS, algorithms and deep learning.</p>
             <img src={sandbox} alt="sandbox" class="responsive-img"></img>
         </div>
     );
